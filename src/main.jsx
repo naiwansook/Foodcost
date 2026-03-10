347f51c79c8baba6b0b2ff241ac185d45a65452b
naiwansook <naiwansook@gmail.com>
2026-03-10 22:39:35 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FoodCostApp from './FoodCostApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FoodCostApp />
+  </React.StrictMode>
+)
